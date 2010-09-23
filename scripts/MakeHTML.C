98f52ad1aaa860b832102cf60cf45d47d9572743
admin <admin@e4a34bab-3947-0410-866c-ab59ef520b34>
2010-09-23 12:20:38 +0000

    change MakeHTML.C loads kpta now instead of the old name kpsa.
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/EdwDataStructure/trunk@717 e4a34bab-3947-0410-866c-ab59ef520b34

--- scripts/MakeHTML.C
+++ scripts/MakeHTML.C
@@ -4,11 +4,11 @@
   gSystem.Load("$KDATA_ROOT/lib/libkds.so");
   gSystem.Load("$KDATA_ROOT/lib/libkera.so");
   gSystem.Load("$KDATA_ROOT/lib/libEra.so");
-  gSystem.Load("$KDATA_ROOT/lib/libkpsa.so");
+  gSystem.Load("$KDATA_ROOT/lib/libkpta.so");
 
   THtml h;
   h.SetProductName("KData");
-  h.SetSourceDir("$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpsa:$KDATA_ROOT/kdsScripts"); //list all module directories like this "./kds:./kpsa:./ktemplate"
+  h.SetSourceDir("$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpta:$KDATA_ROOT/kdsScripts"); //list all module directories like this "./kds:./kpsa:./ktemplate"
   h.SetOutputDir("$KDATA_ROOT/html");
   h->MakeAll();
 }
