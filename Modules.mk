5a581f65eb3c3e8dad6dab7c0e81b40a88e6affc
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2010-11-24 14:51:28 +0000

    This is a template module that could eventually be used as a robust tool for reading Samba data files and sending data outputs to KData raw files.
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/EdwDataStructure/trunk@753 e4a34bab-3947-0410-866c-ab59ef520b34

--- Modules.mk
+++ Modules.mk
@@ -1,6 +1,6 @@
 ##### List of KDATA modules to build #####
 
-MODULES      := kds era 
+MODULES      := kds era ksamba
 
 ### kera depends on kpta now, so it must come afterwards in this list. 
 ifneq ($(FFTW_DIR),)
