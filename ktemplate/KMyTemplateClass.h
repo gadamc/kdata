58ec5c550ecbbc898887944ef4353aad9cf9ec53
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2010-11-24 15:03:51 +0000

    re-adding the template module. it was accidentally deleted somehow.
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/EdwDataStructure/trunk@756 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ ktemplate/KMyTemplateClass.h
@@ -0,0 +1,26 @@
+/*
+ *  KMyTemplateClass.h
+ *  KDataStructure
+ *
+ *  Created by Adam Cox on 3/8/10.
+ *  Copyright 2010 Karlsruhe Institute of Technology. All rights reserved.
+ *
+ */
+#ifndef __KMYTEMPLATECLASS_H__
+#define __KMYTEMPLATECLASS_H__
+
+#include "TTimeStamp.h"
+
+class KMyTemplateClass {
+
+public:
+	KMyTemplateClass(void);
+	virtual ~KMyTemplateClass(void);
+	
+private:
+	
+	ClassDef(KMyTemplateClass, 1);
+};
+
+
+#endif // __KMYTEMPLATECLASS_H__
