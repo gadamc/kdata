8df6de4855d77600e103adf7d2c57115346dab11
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-01-31 17:45:14 +0000

    adding ksamba module to the list of modules that should be added to the HTML reference guide
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/EdwDataStructure/trunk@833 e4a34bab-3947-0410-866c-ab59ef520b34

--- scripts/MakeHTML.C
+++ scripts/MakeHTML.C
@@ -6,10 +6,11 @@
   gSystem.Load("$KDATA_ROOT/lib/libEra.so");
   gSystem.Load("$KDATA_ROOT/lib/libkpta.so");
   gSystem.Load("$KDATA_ROOT/lib/libkqpa.so");
+  gSystem.Load("$KDATA_ROOT/lib/libksamba.so");
 
   THtml h;
   h.SetProductName("KData");
-  h.SetSourceDir("$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpta:$KDATA_ROOT/kdsScripts:$KDATA_ROOT/kqpa"); //list all module directories like this "./kds:./kpsa:./ktemplate"
+  h.SetSourceDir("$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpta:$KDATA_ROOT/kdsScripts:$KDATA_ROOT/kqpa:$KDATA_ROOT/ksamba"); //list all module directories like this "./kds:./kpsa:./ktemplate"
   h.SetOutputDir("$KDATA_ROOT/html");
-  h->MakeAll();
+  h.MakeAll(kTRUE);
 }
