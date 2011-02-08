049cf950513ff7ac5ca55f8ad047d8bd303c2f87
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-02-08 12:07:16 +0000

    updating make html script
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/EdwDataStructure/trunk@876 e4a34bab-3947-0410-866c-ab59ef520b34

--- scripts/MakeHTML.C
+++ scripts/MakeHTML.C
@@ -7,10 +7,11 @@
   gSystem.Load("$KDATA_ROOT/lib/libkpta.so");
   gSystem.Load("$KDATA_ROOT/lib/libkqpa.so");
   gSystem.Load("$KDATA_ROOT/lib/libksamba.so");
+  gSystem.Load("$KDATA_ROOT/lib/libkdatabase.so");
 
   THtml h;
   h.SetProductName("KData");
-  h.SetSourceDir("$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpta:$KDATA_ROOT/kdsScripts:$KDATA_ROOT/kqpa:$KDATA_ROOT/ksamba"); //list all module directories like this "./kds:./kpsa:./ktemplate"
+  h.SetSourceDir("$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpta:$KDATA_ROOT/kdsScripts:$KDATA_ROOT/kqpa:$KDATA_ROOT/ksamba:$KDATA_ROOT/kdatabase"); //list all module directories like this "./kds:./kpsa:./ktemplate"
   h.SetOutputDir("$KDATA_ROOT/html");
   h.MakeAll(kTRUE);
 }
