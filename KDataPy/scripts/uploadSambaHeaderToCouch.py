10eaebef4ab6d273e23b235a9a9d1fe768de3940
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-03-05 14:33:01 +0000

    fixed to handle case where the string given to getBolometerName doesn't end with 'AB', 'CD' or 'GH'. overall, it should be noted, this script is significantly dependent upon the Samba file structure and changes to that structure could result in errors. It's written to be as flexible as possible, but some assumptions had to be made in order to simplify the code
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/EdwDataStructure/trunk@927 e4a34bab-3947-0410-866c-ab59ef520b34

--- KDataPy/scripts/uploadSambaHeaderToCouch.py
+++ KDataPy/scripts/uploadSambaHeaderToCouch.py
@@ -35,8 +35,14 @@ def getBolometerName(astring):
   
   match = re.search('[F]{0,1}ID[0-9]{1,3}', astring)
   if match != None:
-    return re.findall('[F]{0,1}ID[0-9]{1,3}', astring)[0]
-
+    #some strings appear to have a Ch at the end instead of the 
+    #expected AB, CD or GH ending. Must first check for this possibility
+    
+    if (astring[match.end():] == 'AB','CD','GH')[0] == True:
+      return re.findall('[F]{0,1}ID[0-9]{1,3}', astring)[0]
+    else:
+      return astring[match.start():]
+      
   match = re.search('centre',astring)
   if match != None:
     return astring[match.end():].strip('_ ')
