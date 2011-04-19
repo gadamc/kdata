6d295c2ffdb8f0877b2dcfa784abceb2d3c514cd
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-04-19 16:03:40 +0000

    fixed script
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@975 e4a34bab-3947-0410-866c-ab59ef520b34

--- KDataPy/scripts/scpToSps.py
+++ KDataPy/scripts/scpToSps.py
@@ -7,32 +7,34 @@
 import os, subprocess, shlex
 
 def send(item, path = 'kdata/data/current/raw/'):
-'''
-item can be a file or a directory and path is always relative to 
-/sps/edelweis. This script will ONLY work from two particular
-computer. The S7 machine at Modane (134.158.176.27) and the KIT 
-kalinka machine (ikkatrinsrv1.fzk.de). This is because the CC in Lyon
-has created a special account for us to use for non-interactive
-authentication via ssh key exchange. The keys for each of these computer
-have been register with CC and are only valid for connection of the user
-'adam' at Modane and the user 'edwdata' at KIT. If this needs to change,
-or we need to add users, contact CC, or me (adam.cox@kit.edu)
-and they will set us up.
-'''
+  '''
+  item can be a file or a directory and path is always relative to 
+  /sps/edelweis. This script will ONLY work from two particular
+  computer. The S7 machine at Modane (134.158.176.27) and the KIT 
+  kalinka machine (ikkatrinsrv1.fzk.de). This is because the CC in Lyon
+  has created a special account for us to use for non-interactive
+  authentication via ssh key exchange. The keys for each of these computer
+  have been register with CC and are only valid for connection of the user
+  'adam' at Modane and the user 'edwdata' at KIT. If this needs to change,
+  or we need to add users, contact CC, or me (adam.cox@kit.edu)
+  and they will set us up.
+  '''
   option = ''
   if os.path.isdir(item):
     option = '-r' #add the recursive transfer
-  elif !os.path.isfile(item):
+  elif os.path.isfile(item) == False:
     print 'wie bitte? item is neither a directory or a file?'
     return
   
-  pathToSps = 'gadamc@edwdata.in2p3.fr:/sps/edelweis/' + os.path.join('/sps/edelweis/', path)
-  command = 'spc' + ' ' + option + ' ' + item + ' ' + pathToSps
+  pathToSps = 'gadamc@edwdata.in2p3.fr:' + os.path.join('/sps/edelweis/', path)
+  command = 'scp' + ' ' + option + ' ' + item + ' ' + pathToSps
   print command
   args = shlex.split(command)
-  p = subprocess Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  p.wait()
-  for line in p.stderr:
-    print line
-    
+  try:
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    for line in p.stderr:
+      print line
+  except Exception as e:
+    print 'an exception occured', e
     
