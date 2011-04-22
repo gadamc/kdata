6d35c94ad4b32024d128297fe4ff2a628816341a
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-04-22 16:55:48 +0000

    making the kdata reference pages look more "edelweiss" like.
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1004 e4a34bab-3947-0410-866c-ab59ef520b34

--- scripts/MakeHTML.C
+++ scripts/MakeHTML.C
@@ -10,8 +10,21 @@
   gSystem.Load("$KDATA_ROOT/lib/libkdatabase.so");
 
   THtml h;
+  h.SetHeader("scripts/kdata_header.html");
   h.SetProductName("KData");
+  h.SetHomepage("https://edwdev-ik.fzk.de");
   h.SetSourceDir("$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpta:$KDATA_ROOT/kdsScripts:$KDATA_ROOT/kqpa:$KDATA_ROOT/ksamba:$KDATA_ROOT/kdatabase"); //list all module directories like this "./kds:./kpsa:./ktemplate"
   h.SetOutputDir("$KDATA_ROOT/html");
   h.MakeAll(kTRUE);
+  ofstream afile("html/ROOT.css", fstream::in | fstream::out | fstream::ate);
+  afile << 
+  "#kdata_header {" << endl <<
+  "   margin:0px -13px;" << endl <<
+  "}" << endl <<
+  "#kdata_header img {" << endl <<
+  "  margin: 10px;" << endl <<
+  "}" << endl;
+  gSystem.Exec("cp scripts/kdata_header.html html/kdata_header.html");
+  gSystem.Exec("cp scripts/edelweiss-logo.jpeg html/edelweiss-logo.jpeg");
 }
+
