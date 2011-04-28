5412c2f8bf6432ee697cf554269ff455433a596d
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-04-28 14:01:37 +0000

    setup for KJson in place. KCouchDB class is here, but its empty. Will need to fill it in soon. Shouldn't be much work for basic functionality.
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1011 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ kdatabase/KCouchDB.cxx
@@ -0,0 +1,27 @@
+//______________________________________________________________________
+//
+// KCouchDB.cxx
+// Author: Adam Cox <mailto:adam.cox@kit.edu>
+//
+// *Copyright 2011 Karlsruhe Inst. of Technology. All Rights Reserved.
+//
+//
+// Created Thursday 28. April 2011
+//
+//
+
+
+#include "KCouchDB.h"
+
+ClassImp(KCouchDB);
+
+KCouchDB::KCouchDB(void)
+{
+
+}
+
+KCouchDB::~KCouchDB(void)
+{
+
+
+}
