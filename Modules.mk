840ca91c2e40caade5f365db8b631e080c08542f
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-05-28 17:00:14 +0000

    cherry picking update from v4.0
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1036 e4a34bab-3947-0410-866c-ab59ef520b34

--- Modules.mk
+++ Modules.mk
@@ -7,6 +7,6 @@ ifneq ($(FFTW_DIR),)
 MODULES      += kpta kera kqpa
 endif
 
-MODULES += $(if $(and $(CURL_DIR),$(JANSSON_DIR)),kdatabase,)
+MODULES += $(if $(and $(CURL_DIR),kdatabase,)
 
-#MODULES += $(if $(and $(CURL_DIR),$(JANSSON_DIR),$(FFTW_DIR)),kdsplay,)
+#MODULES += $(if $(and $(CURL_DIR),$(FFTW_DIR)),kdsplay,)
