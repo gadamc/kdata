cc0d9c1091de58cf6c25f47a539ec2b1ef9d1d25
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-05-28 16:40:49 +0000

    adding kqpa module back into the Modules.mk file since the update was recently made.
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1033 e4a34bab-3947-0410-866c-ab59ef520b34

--- Modules.mk
+++ Modules.mk
@@ -4,7 +4,7 @@ MODULES      := kds era ktemplate ksamba
 
 ### kera depends on kpta now, so it must come afterwards in this list. 
 ifneq ($(FFTW_DIR),)
-MODULES      += kpta kera 
+MODULES      += kpta kera kqpa
 endif
 
 MODULES += $(if $(and $(CURL_DIR),$(JANSSON_DIR)),kdatabase,)
