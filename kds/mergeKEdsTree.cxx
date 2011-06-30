ac2293edfad973845dd0826f6185370f483d2f50
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-06-30 16:10:27 +0000

    ugg.. now it works
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1135 e4a34bab-3947-0410-866c-ab59ef520b34

--- kds/mergeKEdsTree.cxx
+++ kds/mergeKEdsTree.cxx
@@ -471,7 +471,7 @@ Int_t mergeKEdsTree(string inputPath1, string inputPath2, string outputPath, str
 	cout << "File 1"<< endl;
 
 	
-	KDataWriter f(outputPath.c_str() "KHLAEvent");
+	KDataWriter f(outputPath.c_str(), "KHLAEvent");
 	KHLAEvent *mEv = dynamic_cast<KHLAEvent *>(f.GetEvent());
 	if(mEv == 0) return -1;
 	f.GetTTree()->BranchRef(); //creates an extra Branch table filled with Branches that have TRefs or TRefArrays
