b9d327d007db6dc268ed623d59b8bed2138eb9f5
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-08-01 12:04:27 +0000

    renaming tier to proc
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1168 e4a34bab-3947-0410-866c-ab59ef520b34

--- KDataPy/scripts/addProc.py
+++ KDataPy/scripts/addProc.py
@@ -21,41 +21,41 @@ def formatSrbYearMonthDirStructure(sambaDirName):
   basename = os.path.basename(sambaDirName)
   return str(getyeardir(basename)) + '/' + getmonthdir(basename) + '{0:02d}'.format((getyeardir(basename) - 2000)) + '/events'
 
-def addDocsFromView(vr, tiername):
+def addDocsFromView(vr, procname):
   global db
        
   for row in vr:
-    print 'adding tier to', row['id']
+    print 'adding proc to', row['id']
     doc = db.get(row['id'])
-    tierdict = dict()
-    if tiername == 'tier0':
-      tierdict['processname'] = 'rootifyAndCopyToSps'
-      tierdict['file'] = '/sps/edelweiss/kdata/data/current/raw/' + os.path.basename(doc['file']) + '.root'
-      tierdict['hostname'] = 'ccali.in2p3.fr'
-      tierdict['command'] = '/usr/bin/scp '  + doc['file'] + ' gadamc@edwdata.in2p3.fr:/sps/edelweiss/kdata/data/current/raw/' + os.path.basename(tierdict['file'])
-      tierdict['scpErrs'] = list()
-      tierdict['uname'] = os.uname()
+    procdict = dict()
+    if procname == 'proc0':
+      procdict['processname'] = 'rootifyAndCopyToSps'
+      procdict['file'] = '/sps/edelweiss/kdata/data/current/raw/' + os.path.basename(doc['file']) + '.root'
+      procdict['hostname'] = 'ccali.in2p3.fr'
+      procdict['command'] = '/usr/bin/scp '  + doc['file'] + ' gadamc@edwdata.in2p3.fr:/sps/edelweiss/kdata/data/current/raw/' + os.path.basename(procdict['file'])
+      procdict['scpErrs'] = list()
+      procdict['uname'] = os.uname()
     else:
-      print 'only supporting tier0'
+      print 'only supporting proc0'
       sys.exit(-1)
     
     doc['status'] = 'good'
-    doc[tiername] = tierdict
+    doc[procname] = procdict
     db.save_doc(doc)
 
   
-def addTierToRun(uri, dbname, tiername, runname):
+def addProcToRun(uri, dbname, procname, runname):
   global db
   vr = db.view('proc/daqdoc', reduce = False, key = runname)
 
-  addDocsFromView(vr, tiername)
+  addDocsFromView(vr, procname)
   
   
-def addTierToRangeOfRuns(uri, dbname, tiername, startrun, endrun):
+def addProcToRangeOfRuns(uri, dbname, procname, startrun, endrun):
   global db
   vr = db.view('proc/daqdoc',  reduce = False, startkey = startrun, endkey = endrun)
   
-  addDocsFromView(vr, tiername)
+  addDocsFromView(vr, procname)
   
 def main(*args):
   
@@ -65,7 +65,7 @@ def main(*args):
     
   uri = args[0]
   dbname = args[1]
-  tiername = args[2]
+  procname = args[2]
 
   global db
   s = Server(uri)
@@ -73,12 +73,12 @@ def main(*args):
 
   if len(args) == 4:
     runname = args[3]
-    addTierToRun(uri, dbname, tiername, runname)
+    addProcToRun(uri, dbname, procname, runname)
   
   elif len(args) == 5:
     startrun = args[3]
     endrun = args[4]
-    addTierToRangeOfRuns(uri, dbname, tiername, startrun, endrun)
+    addProcToRangeOfRuns(uri, dbname, procname, startrun, endrun)
 
 if __name__ == '__main__':
   main(*sys.argv[1:])
