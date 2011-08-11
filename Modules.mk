26e07526ab7c4c9fbf477dc0fc0a061761528fc6
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-08-11 09:10:04 +0000

    test
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1184 e4a34bab-3947-0410-866c-ab59ef520b34

--- Modules.mk
+++ Modules.mk
@@ -5,6 +5,7 @@ MODULES      := kds era ksamba kebap
 ### kera depends on kpta now, so it must come afterwards in this list. 
 ifneq ($(FFTW_DIR),)
 MODULES      += kpta kera kqpa ktemplate
+
 endif
 
 MODULES += $(if $(CURL_DIR),kdatabase,)
