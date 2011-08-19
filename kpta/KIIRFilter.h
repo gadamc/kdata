422618f746b0bbf7a63acbe984cd9ba6761d0675
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-08-19 11:53:39 +0000

    first commit of the filters... oh and some example program in the ktemplate lib
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1214 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ kpta/KIIRFilter.h
@@ -0,0 +1,36 @@
+//______________________________________________________________________
+//
+// KIIRFilter.h
+// Author: Adam Cox <mailto:adam.cox@kit.edu>
+//
+// *Copyright 2010 Karlsruhe Inst. of Technology. All Rights Reserved
+//
+//
+
+
+#ifndef __KIIRFILTER_H__
+#define __KIIRFILTER_H__
+
+#include "KPtaProcessor.h"
+
+class KIIRFilter : public KPtaProcessor { 
+  
+public:
+  //Constructors
+  KIIRFilter(void);
+  virtual ~KIIRFilter(void);
+  
+	virtual bool RunProcess(void);
+	
+  //getters
+  
+  //setters
+private:
+  
+  //private methods
+  void InitializeMembers(void);
+  
+};
+
+
+#endif
