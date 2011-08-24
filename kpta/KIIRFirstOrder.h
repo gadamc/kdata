3f8d5448b0d4a7101153f49bd72256edd7c769e2
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-08-24 17:13:26 +0000

    adding new constructor for the various processors. adding iir first order filter... its not done yet though
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1232 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ kpta/KIIRFirstOrder.h
@@ -0,0 +1,39 @@
+//______________________________________________________________________
+//
+// KIIRFirstOrder.h
+// Author: Adam Cox <mailto:adam.cox@kit.edu>
+//
+// *Copyright 2010 Karlsruhe Inst. of Technology. All Rights Reserved
+//
+//
+
+
+#ifndef __KIIRFIRSTORDER_H__
+#define __KIIRFIRSTORDER_H__
+
+#include "KIIRFilter.h"
+
+class KIIRFirstOrder : public KIIRFilter { 
+  
+public:
+  //Constructors
+  KIIRFirstOrder(void);
+  KIIRFirstOrder(double a1, double b0, double b1);
+  void SetCoefA(double a1);  
+  void SetCoefB(double b0, double b1);  
+  
+  virtual ~KIIRFirstOrder(void);
+  
+	virtual bool RunProcess(void);
+  
+private:
+  
+  //private methods
+  void InitializeMembers(void);
+  
+ // ClassDef(KIIRFirstOrder,1);
+  
+};
+
+
+#endif
