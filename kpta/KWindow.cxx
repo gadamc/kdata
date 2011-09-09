46b3590688c1dd8d344d1479fde868db07c13f45
munrau <munrau@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-09-09 14:22:10 +0000

    Adding KWindow class. At the moment only the Tukey window function is implemented
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1283 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ kpta/KWindow.cxx
@@ -0,0 +1,89 @@
+//______________________________________________________________________
+//
+// KWindow.cxx
+// Author: Michael Unrau
+//
+// *Copyright 2011 Karlsruhe Inst. of Technology. All Rights Reserved.
+//
+//
+// KWindow class can be used to scale a pulse with a desired window function (at the moment only the Tukey window is implemented)
+// To use it you should call the SetTukeyWindow(double alpha) function before processing, the alpha parameter defines the shape
+// of the window: alpha = 0 is a rectangular window (pulse remains unchanged), alpha = 1 is a Hann window
+
+#include "KWindow.h"
+#include <cmath>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+//ClassImp(KWindow);
+
+KWindow::KWindow(void)
+{
+  SetName("KWindow");
+  InitializeMembers();
+}
+
+KWindow::KWindow(double *inPulse, unsigned int inSize, double* outPulse, unsigned int outsize)
+  : KPtaProcessor(inPulse, inSize, outPulse, outsize)
+{
+   SetName("KWindow"); 
+   InitializeMembers();
+}
+
+KWindow::~KWindow(void)
+{
+  if(fCoef) delete [] fCoef;
+}
+
+void KWindow::SetTukeyWindow(double alpha)
+{
+  if(fInputSize != fCoefSize){
+    if(fCoef) delete [] fCoef;
+    fCoefSize = fInputSize;
+    fCoef = new double[fCoefSize];
+  }
+  if(fCoefSize != 0){
+		double  temp = alpha*(fCoefSize-1)/2.0;
+		for(int i = 0; i < (int) temp; i++)
+			*(fCoef+i) = 0.5 + 0.5*cos(3.14159265358979*((i/(double)temp) - 1));
+		for(unsigned int i = (unsigned int) temp; i < (unsigned int) (fCoefSize-temp); i++)
+			*(fCoef+i) = 1.0;
+		for(unsigned int i = (unsigned int) (fCoefSize-temp); i < fCoefSize; i++)
+			*(fCoef+i) = 0.5 + 0.5*cos(3.14159265358979*((i/(double) temp) - 2.0/alpha - 1));		
+	}
+  
+}
+
+bool KWindow::RunProcess(void)
+{
+  if(fInputPulse == 0 || fOutputPulse == 0) {
+    cerr << "input and output pulses are not allocated." << endl;
+    return false;
+  }
+  
+  if(fCoefSize == 0){
+		cerr<<"window coefficients are not set"<<endl;
+		return false;
+	}
+  //clear the output pulse
+  memset(fOutputPulse, 0, fInputSize*sizeof(double));
+
+  fOutputSize = fInputSize;
+    
+  for(unsigned int i = 0 ; i < fOutputSize; i++)
+    *(fOutputPulse+i) =  *(fInputPulse+i) * *(fCoef+i); 
+ 
+  return true;
+}
+
+
+
+void KWindow::InitializeMembers(void)
+{
+  fCoef = 0;
+  fCoefSize = 0;
+  
+}
+
+
---
+++ kpta/KWindow.h
@@ -0,0 +1,47 @@
+//______________________________________________________________________
+//
+// KWindow.h
+// Author: Michael Unrau
+//
+// *Copyright 2011 Karlsruhe Inst. of Technology. All Rights Reserved
+//
+//
+
+
+#ifndef __KWINDOW_H__
+#define __KWINDOW_H__
+
+#include "KPtaProcessor.h"
+#include <string.h>
+
+class KWindow : public KPtaProcessor { 
+  
+public:
+  //Constructors
+  KWindow(void);
+  //for the memory-savy programmers
+  KWindow(double *inPulse, unsigned int inSize, double* outPulse, unsigned int outsize);
+  
+  virtual ~KWindow(void);
+  
+	virtual bool RunProcess(void);
+	
+  void SetTukeyWindow(double alpha);
+
+  virtual double* GetCoefficients(void) {return fCoef;}
+  virtual unsigned int GetCoefSize(void) {return fCoefSize;}
+
+protected:
+  double *fCoef;
+  unsigned int fCoefSize;
+    
+private:
+  //private methods
+  void InitializeMembers(void);
+  
+ // ClassDef(KWindow,1);
+  
+};
+
+
+#endif
