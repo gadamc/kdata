6a9a6eb7d4872bc25672b6a4ed08eb1582af1066
munrau <munrau@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-09-19 16:23:29 +0000

    Adding a trapezoidal filter, which converts an exponential pulse with a known decay time to a trapezoidal shape pulse
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1315 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ kpta/KTrapezoidalFilter.h
@@ -0,0 +1,49 @@
+//______________________________________________________________________
+//
+// KTrapezoidalFilter.h
+// Author: Michael Unrau
+//
+// *Copyright 2011 Karlsruhe Inst. of Technology. All Rights Reserved
+//
+//
+
+
+#ifndef __KTRAPEZOIDALFILTER_H__
+#define __KTRAPEZOIDALFILTER_H__
+
+#include "KPtaProcessor.h"
+#include <string.h>
+
+class KTrapezoidalFilter : public KPtaProcessor { 
+  
+public:
+  //Constructors
+  KTrapezoidalFilter(void);
+  //for the memory-savy programmers
+  KTrapezoidalFilter(double *inPulse, unsigned int inSize, double* outPulse, unsigned int outsize);
+  
+  virtual ~KTrapezoidalFilter(void);
+  
+	virtual bool RunProcess(void);
+	
+  void SetParams(double DecayTimeConstant, unsigned int RiseTime, unsigned int FlatTopWidth);
+
+  virtual double GetDecayTimeConstant(void) {return fDecayTimeConstant;}
+  virtual unsigned int GetRiseTime(void) {return fRiseTime;}
+  virtual unsigned int GetFlatTopWidth(void) {return fFlatTopWidth;}
+
+protected:
+  double fDecayTimeConstant;
+  unsigned int fRiseTime;
+  unsigned int fFlatTopWidth;
+  
+private:
+  //private methods
+  void InitializeMembers(void);
+  
+ // ClassDef(KTrapezoidalFilter,1);
+  
+};
+
+
+#endif
