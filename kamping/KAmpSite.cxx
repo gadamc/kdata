a11ac1fc55fb113139e5260f3dd13d63e25ccd6b
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-09-22 11:56:06 +0000

    first commit of the kamping library. Added KAmper and KAmpSite abstract classes.
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1317 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ kamping/KAmpSite.cxx
@@ -0,0 +1,22 @@
+//
+// KAmpSite.cxx  
+// KDataStructure
+//
+// Created by Adam Cox on 3/8/10.
+// Copyright 2011 Karlsruhe Institute of Technology. All rights reserved.
+
+//
+
+#include "KAmpSite.h"
+
+ClassImp(KAmpSite);
+
+KAmpSite::KAmpSite(void)
+{
+
+}
+
+KAmpSite::~KAmpSite(void)
+{
+
+}
---
+++ kamping/KAmper.cxx
@@ -0,0 +1,22 @@
+//
+// KAmper.cxx  
+// KDataStructure
+//
+// Created by Adam Cox on 3/8/10.
+// Copyright 2011 Karlsruhe Institute of Technology. All rights reserved.
+
+//
+
+#include "KAmper.h"
+
+ClassImp(KAmper);
+
+KAmper::KAmper(void)
+{
+
+}
+
+KAmper::~KAmper(void)
+{
+
+}
