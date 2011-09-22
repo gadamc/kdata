a99652698de238b79d87a731572a323887b167d6
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-09-22 12:23:14 +0000

    updaing make html script.
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1320 e4a34bab-3947-0410-866c-ab59ef520b34

--- kamping/KAmpSite.cxx
+++ kamping/KAmpSite.cxx
@@ -5,7 +5,12 @@
 // Created by Adam Cox
 // Copyright 2011 Karlsruhe Institute of Technology. All rights reserved.
 
-//
+// A KAmpSite is responsible for taking Raw Pulse traces, passing those pulses to its KAmpers to 
+// estimate the pulse amplitudes and peak positions, then to fill the KAmpEvents with the appropriate
+// results. The appropriate results can be any number of KPulseAnalysisRecords added to the KAmpEvent.
+// A KAmpSite will be used in a routine or program that's main goal is to take a RAW-level KData file and
+// convert it into an AMP-level KData file that is filled with pulse characteristic estimates (amplitudes, start times,
+// pulse widths, chi^2, etc...). 
 
 #include "KAmpSite.h"
 
--- scripts/MakeHTML.C
+++ scripts/MakeHTML.C
@@ -8,6 +8,7 @@
   gSystem.Load("$KDATA_ROOT/lib/libkqpa.so");
   gSystem.Load("$KDATA_ROOT/lib/libksamba.so");
   gSystem.Load("$KDATA_ROOT/lib/libkdatabase.so");
+  gSystem.Load("$KDATA_ROOT/lib/libkamping.so");
 
   THtml h;
   gSystem.Exec("rm -rf $KDATA_ROOT/html");  //completely remove the html folder so that all 
@@ -15,7 +16,7 @@
   h.SetHeader("scripts/kdata_header.html");
   h.SetProductName("KData");
   h.SetHomepage("https://edwdev-ik.fzk.de");
-  h.SetSourceDir("$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpta:$KDATA_ROOT/kdsScripts:$KDATA_ROOT/kqpa:$KDATA_ROOT/ksamba:$KDATA_ROOT/kdatabase"); //list all module directories like this "./kds:./kpsa:./ktemplate"
+  h.SetSourceDir("$KDATA_ROOT/kamping:$KDATA_ROOT/kds:$KDATA_ROOT/kera:$KDATA_ROOT/era:$KDATA_ROOT/kpta:$KDATA_ROOT/kdsScripts:$KDATA_ROOT/kqpa:$KDATA_ROOT/ksamba:$KDATA_ROOT/kdatabase"); //list all module directories like this "./kds:./kpsa:./ktemplate"
   h.SetOutputDir("$KDATA_ROOT/html");
   h.MakeAll(kTRUE);
   ofstream afile("html/ROOT.css", fstream::in | fstream::out | fstream::ate);
