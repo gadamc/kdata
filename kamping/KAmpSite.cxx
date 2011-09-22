b2844bad700ffd58f99b1d03d313f2a1be3ee96f
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2011-09-22 12:35:46 +0000

    adding some documentation
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1322 e4a34bab-3947-0410-866c-ab59ef520b34

--- kamping/KAmpSite.cxx
+++ kamping/KAmpSite.cxx
@@ -4,7 +4,7 @@
 //
 // Created by Adam Cox
 // Copyright 2011 Karlsruhe Institute of Technology. All rights reserved.
-
+//
 // A KAmpSite is responsible for taking Raw Pulse traces, passing those pulses to its KAmpers to 
 // estimate the pulse amplitudes and peak positions, then to fill the KAmpEvents with the appropriate
 // results. The appropriate results can be any number of KPulseAnalysisRecords added to the KAmpEvent.
