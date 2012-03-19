2ead57c7dc61a3580ce718b965e450576411f352
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2012-03-19 10:39:11 +0000

    adding back kampsite with the correct file name
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1558 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ kamping/KChamonixKAmpSite.h
@@ -0,0 +1,83 @@
+//
+// KChamonixKAmpSite.h
+// KDataStructure
+//
+// Created by Adam Cox
+// Copyright 2011 Karlsruhe Institute of Technology. All rights reserved.
+//
+//
+
+#ifndef __KCHAMONIXKAMPSITE_H_
+#define __KCHAMONIXKAMPSITE_H_
+
+#include "KAmpSite.h"
+#include "KOptimalKamper.h"
+#include "KPeakDetectorProto.h"
+#include "KRealToHalfComplexDFT.h"
+#include "KHalfComplexPower.h"
+#include "KBaselineRemoval.h"
+#include "KWindow.h"
+#include "KEraPeakFinder.h"
+#include <map>
+#include <string>
+#include <vector>
+
+class KAmpEvent;
+class KRawEvent;
+class KAmpBolometerRecord;
+class KAmpBoloPulseRecord;
+class KRawBoloPulseRecord;
+
+class KChamonixKAmpSite : public KAmpSite {
+
+public:
+  KChamonixKAmpSite(void);
+  virtual ~KChamonixKAmpSite(void);
+  
+  virtual Bool_t RunKampSite(KRawBolometerRecord *boloRaw, KAmpBolometerRecord *boloAmp, KAmpEvent *ee); //this runs the data processing on each event. 
+  virtual Bool_t ScoutKampSite(KRawBoloPulseRecord* pRaw, KRawEvent *e);  //should call this method first. Use this to scan through data to estimate noise...etc..
+  virtual Bool_t NeedScout(void){ return true;}
+  
+  //methods to set internal values for the various parameters for specific channels.
+  //these parameters should be available from the database 
+  Bool_t SetTemplate(const char* channelName,  std::vector<double>& pulse);
+  //std::vector<double> GetTemplateSpectrum(const char* channelName) const;
+  
+  void SetTrapDecayConstant(const char* channelName, double value){fDecayValues[channelName] = value;}
+  double GetTrapDecayConstant(const char* channelName) const;
+  
+  unsigned int GetNumNoiseEventsFound(const char* channelName) const;
+  //std::vector<double> GetNoisePower(const char* channelName) const;
+  
+  KBaselineRemoval& GetBaselineRemovalHeat(void){return fBaselineRemovalHeat;}
+  KWindow& GetHeatWindow(void){return fHeatWindow;}
+  
+  KOptimalKamper& GetOptimalKamper(void){return fOptKamper;}
+  KRealToHalfComplexDFT& GetRealToHalfComplexDFT(void){return fR2Hc;}
+  KHalfComplexPower& GetHalfComplexPower(void){return fHc2P;}
+  
+  
+private:
+  
+  //KPeakDetectorProto fIonPeakDetector;
+  //KPeakDetectorProto fHeatPeakDetector;
+  KEraPeakFinder fHeatPeakDetector;
+  
+  KBaselineRemoval fBaselineRemovalHeat;
+  //KBaselineRemoval fBaselineRemovalIon;
+  
+  KRealToHalfComplexDFT fR2Hc;
+  KHalfComplexPower fHc2P;
+  KWindow fHeatWindow;
+  
+  std::map<std::string, unsigned int> fNoiseEventCounts;
+  std::map<std::string, std::vector<double> > fNoiseSpectra;
+  std::map<std::string, std::vector<double> > fTemplateSpectra;
+  std::map<std::string, double> fDecayValues;
+  
+  KOptimalKamper fOptKamper;
+  
+};
+
+
+#endif // __KCHAMONIXKAMPSITE_H_
