2a626bd9647f52c37a000f8b4fdcaecdc4cec076
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2012-05-15 09:44:12 +0000

    Squashed commit of the following:
    breaking up the kampkounselor runkamp method into three separate methods in order to let a user better control this process. This may be useful, for example, if the user wishes to use python libraries (such as scipy) to perform scouting analysis.
    
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1600 e4a34bab-3947-0410-866c-ab59ef520b34

--- kamping/KAmpKounselor.cxx
+++ kamping/KAmpKounselor.cxx
@@ -30,7 +30,8 @@ KAmpKounselor::KAmpKounselor(void)
 
 KAmpKounselor::~KAmpKounselor(void)
 {
-
+  if(fInput) delete fInput;
+  if(fOutput) delete fOutput;
 }
 
 void KAmpKounselor::AddKAmpSite(KAmpSite *aSite)
@@ -39,28 +40,43 @@ void KAmpKounselor::AddKAmpSite(KAmpSite *aSite)
   
 }
 
-Bool_t KAmpKounselor::RunKamp(const char* inputRawKDataFile, const char* outputAmpKDataFile)
+void KAmpKounselor::SetFiles(const char* inputRawKDataFile, const char* outputAmpKDataFile)
 {
-  KDataReader f(inputRawKDataFile);
-  KDataWriter ff(outputAmpKDataFile,"KAmpEvent");
-  KAmpEvent *ee = (KAmpEvent *)ff.GetEvent();
-  KRawEvent *e = (KRawEvent *)f.GetEvent();
+  if(fInput) delete fInput;
+  fInput = new KDataReader(inputRawKDataFile);
   
-  f.cd();
-  int numEvents = f.GetEntries();
+  if(fOutput) delete fOutput;
+  fOutput = new KDataWriter(outputAmpKDataFile, "KAmpEvent");
   
-  KDataProcessingInfo *mNewInfo = (KDataProcessingInfo *)(ff.GetTTree()->GetUserInfo()->Last());
-  mNewInfo->AddModule("KAmpKounselor");
-  
-  
+}
+
+Bool_t KAmpKounselor::CheckSetup()
+{
+  if (fInput==0 || fOutput == 0) return false;
+  else return true;
+}
+
+Bool_t KAmpKounselor::Scout()
+{
   //check to see if any of the KAmpSites that are managed by this KAmpKounselor need
-  //to "scout" their kampsite. For sure, a KAmpSite that employs an Optimal Filter
+  //to "scout" their kampsite. For example, a KAmpSite that employs an Optimal Filter
   //will need to scout out the data in order to estimate the noise power for each channel.
+  
+  if(!CheckSetup()) return false;
+  
+  fInput->cd();
+  int numEvents = fInput->GetEntries();
+  KRawEvent *e = (KRawEvent *)fInput->GetEvent();
+  
   vector<KAmpSite *>::iterator it;
   Bool_t bNeedScouting = false;
   
+  KDataProcessingInfo *fNewInfo=0;
+  fNewInfo = (KDataProcessingInfo *)(fOutput->GetTTree()->GetUserInfo()->Last());
+  fNewInfo->AddModule("KAmpKounselor");
+  
   for( it = fKampSites.begin(); it < fKampSites.end(); it++){
-    mNewInfo->AddCommand( (*it)->GetName());
+    fNewInfo->AddCommand( (*it)->GetName());
     if ( (*it)->NeedScout()){
       bNeedScouting = true;
       break;
@@ -73,7 +89,7 @@ Bool_t KAmpKounselor::RunKamp(const char* inputRawKDataFile, const char* outputA
   //to be used in the optimal filter
   if(bNeedScouting){
     for(int i = 0; i < numEvents; i++){
-      f.GetEntry(i);
+      fInput->GetEntry(i);
       for(int j = 0; j < e->GetNumBoloPulses(); j++){
         KRawBoloPulseRecord *pRaw = (KRawBoloPulseRecord *)e->GetBoloPulse(j);
         for( it = fKampSites.begin(); it < fKampSites.end(); it++){
@@ -84,18 +100,44 @@ Bool_t KAmpKounselor::RunKamp(const char* inputRawKDataFile, const char* outputA
     }
   }
   
+  return true;
+}
+
+Bool_t KAmpKounselor::Prepare()
+{
   //call each KampSite's Prepare method, which is used to calculate any information
-  //obtained during the ScoutKampSite method
+  //obtained during the ScoutKampSite method that will be needed before running the analysis
+  //on the data.
+
+  if(!CheckSetup()) return false;
+
+  vector<KAmpSite *>::iterator it;
+
   for( it = fKampSites.begin(); it < fKampSites.end(); it++){
     if ( (*it)->NeedScout() )
       (*it)->PrepareKampSite();
   }
   
-  
+  return true;
+}
+
+Bool_t KAmpKounselor::Run()
+{
   //loop through the data, passing data to the KAmpSites in order to calculate
-  //pulse amplitudes.
+  //pulse amplitudes and other characteristics of each event.
+  
+  if(!CheckSetup()) return false;
+    
+  KAmpEvent *ee = (KAmpEvent *)fOutput->GetEvent();
+  KRawEvent *e = (KRawEvent *)fInput->GetEvent();
+  
+  vector<KAmpSite *>::iterator it;
+  
+  fInput->cd();
+  int numEvents = fInput->GetEntries();
+  
   for(int ii = 0; ii < numEvents; ii++){
-    f.GetEntry(ii);
+    fInput->GetEntry(ii);
     
     ee->Clear("C");
     *(KEvent *)ee = *(KEvent *)e; //copy the base-class stuff
@@ -116,12 +158,25 @@ Bool_t KAmpKounselor::RunKamp(const char* inputRawKDataFile, const char* outputA
       
     }
     
-    ff.Fill();
+    fOutput->Fill();
   }
   
-  int writeReturn = ff.Write();
-  ff.Close();
-  f.Close();
+  int writeReturn = fOutput->Write();
+  fOutput->Close();
+  fInput->Close();
   return writeReturn;
+}
 
+
+Bool_t KAmpKounselor::RunKamp(const char* inputRawKDataFile, const char* outputAmpKDataFile)
+{
+  //calls SetFiles, Scout, Prepare and then Run. Returns the result of Run.
+  
+  SetFiles(inputRawKDataFile, outputAmpKDataFile);
+  
+  if(!Scout()) return false;
+  
+  if(!Prepare()) return false;
+  
+  return Run();
 }
--- kamping/KAmpKounselor.h
+++ kamping/KAmpKounselor.h
@@ -14,6 +14,8 @@
 #include <vector>
 
 class KAmpSite;
+class KDataReader;
+class KDataWriter;
 
 class KAmpKounselor {
 
@@ -22,11 +24,20 @@ public:
   KAmpKounselor(void);
   virtual ~KAmpKounselor(void);
   virtual void AddKAmpSite(KAmpSite *aSite);
+  virtual void SetFiles(const char* inputRawKDataFile, const char* outputAmpKDataFile);
   virtual Bool_t RunKamp(const char* inputRawKDataFile, const char* outputAmpKDataFile);
+  virtual Bool_t Scout();
+  virtual Bool_t Prepare();
+  virtual Bool_t Run();
   //virtual std::vector<KAmpSite *> GetListOfKAmpSites(void){ return fKampSites;}
   
 private:
   std::vector<KAmpSite *> fKampSites;
+  KDataReader *fInput;
+  KDataWriter *fOutput;
+  
+  Bool_t CheckSetup();
+
 };
 
 
