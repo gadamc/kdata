9b89b54d1340ec4926e5e89e8a8bec7ba6df24a5
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2012-06-22 12:16:31 +0000

    adding more sophisiticated looping routines - complete with callback functions for user-defined analysis
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1615 e4a34bab-3947-0410-866c-ab59ef520b34

--- KDataPy/util.py
+++ KDataPy/util.py
@@ -2,11 +2,14 @@ import numpy as np
 from ROOT import *
 import KDataPy
 import matplotlib.pyplot as plt
+import copy
+
 plt.ion()
 
 try:
-  gSystem.Load('libkpta')
   gSystem.Load('libkds')
+  gSystem.Load('libkpta')
+  gSystem.Load('libkamping')
  
 except Exception as e: 
   print 'failed to load KDataPy.util'
@@ -34,7 +37,90 @@ def get_in(pta):
   '''
   return get_as_nparray(pta.GetInputPulse(), pta.GetInputPulseSize())
     
-def plotpulse(data, name=None, match=False, pta = None, analysisFunction = None):
+
+
+def looppulse(data, name=None, match=False, pta=None, analysisFunction = None, **kwargs):
+  '''
+  Like plotpulse, but no plotting for each pulse. It just loops through each pulse record for you
+  and you provide the analysis function. 'pta' is a KPulseProcessor. If pta != None, then it will
+  call pta.SetInputPulse and pta.RunProcess for you.
+  
+  The analysisFunciton takes two arguments, a KPulseRecord (may be Raw, Amp or HLA), and the 'pta'.
+  example, plus optional set of kwargs, which are passed in above
+  
+  def myFunction(pulseRecord, ptaObject=None):
+    print pulseRecord.GetChannelName()
+    if ptaObject != None:
+      return get_out( ptaObject )
+  
+  bas = KBaselineRemoval()
+  
+  util.looppulse('path/to/kdatafile.root', name='ZM1', pta = bas, analysisFunction = myFunction)
+  
+  '''
+  if 'KDataReader' in str(type(data)): kdfilereader = data
+  else:
+    kdfilereader = KDataPy.KDataReader(data)
+    
+  for event in kdfilereader:
+    for pulse in event.boloPulseRecords():
+      
+      if pulse.GetPulseLength() == 0: continue
+      if name:
+        if name not in pulse.GetChannelName(): continue
+        if match==True and name != pulse.GetChannelName(): continue
+      
+      if pta:
+        pta.SetInputPulse( pulse.GetTrace() )
+        pta.RunProcess()
+        
+      if analysisFunction:
+        analysisFunction(pulse, pta, **kwargs)
+
+   
+def loopbolo(data, name=None, match=False, analysisFunction = None, **kwargs):
+  '''
+  Like looppulse, but just loops through each bolo record for you
+  and you provide the analysis function. 
+  "name" should be the name of the bolometer.
+  if match == True, then only an exact match is passed to the analysisFunction
+  The analysisFunction must take one argument, which is of type KBolometerRecord (or subclass), plus optional kwargs
+  
+  For example
+  
+  def myFunction(boloRecord, **kwargs):
+    print boloRecord.GetDetectorName()
+    
+    for pulse in boloRecord.pulseRecords():
+      print pulse.GetChannelName()
+      .... do some analysis....
+      
+      
+  util.loopbolo('/path/to/file.root', name="FID", analysisFunction=myFunction)
+  
+  will loop through the file and return to you any bolometer with a GetDetectorName that
+  contains "FID" and pass that bolo record to your myFunction(bolo).
+  
+  '''
+  if analysisFunction==None:
+    print 'You must provide an analysis function'
+    return
+    
+  if 'KDataReader' in str(type(data)): kdfilereader = data
+  else:
+    kdfilereader = KDataPy.KDataReader(data)
+    
+  for event in kdfilereader:
+    for bolo in event.boloRecords():
+      
+      if name:
+        if name not in bolo.GetDetectorName(): continue
+        if match==True and name != bolo.GetChannelName(): continue
+        
+      analysisFunction(bolo, **kwargs)
+
+            
+def plotpulse(data, name=None, match=False, pta = None, analysisFunction = None, **kwargs):
     '''
     This function provides you with a simple single-plot viewer and some automatic pulse processing. It will loop through a data
     file and can plot each pulse that you specify. Additionally, it can process each pulse with a KPtaProcessor, plotting
@@ -89,35 +175,39 @@ def plotpulse(data, name=None, match=False, pta = None, analysisFunction = None)
       
     
     '''
-    if 'KDataReader' in str(type(data)): kdfilereader = data
-    else:
-      kdfilereader = KDataPy.KDataReader(data)
+        
+    def plotingfunction(pulse, pta=None, **kwargs):
+            
       
-    print 'type "quit" at any time to exit'
-    for event in kdfilereader:
-      for pulse in event.boloPulseRecords():
+      print 'plotting', pulse.GetChannelName()
+      if pta != None:
+        pta.SetInputPulse( pulse.GetTrace() )
+        pta.RunProcess()
+        plt.plot( get_out(pta) )
+      else:
+        plt.plot( np.array(pulse.GetTrace()) )
         
-        if pulse.GetPulseLength() == 0: continue
-        if name not in pulse.GetChannelName(): continue
-        if match==True and name != pulse.GetChannelName(): continue
+      if kwargs['callersAnalysisFunction']:
+        newkwargs = copy.deepcopy(kwargs)
+        del newkwargs['callersAnalysisFunction']
+        kwargs['callersAnalysisFunction'](pulse,pta=pta, **newkwargs)
+      
+      try:
+        if raw_input() == 'quit': 
+          plt.cla()
+          raise KeyboardInterrupt
+      except KeyboardInterrupt: 
+        plt.cla()
+        raise KeyboardInterrupt
+      
+      plt.cla()
+      
+      
+    looppulse(data, name=name, match=match, pta = pta, analysisFunction = plotingfunction, callersAnalysisFunction = analysisFunction, **kwargs)  
+    
         
-        print 'plotting', pulse.GetChannelName()
-        if pta != None:
-          pta.SetInputPulse( pulse.GetTrace() )
-          pta.RunProcess()
-          plt.plot( get_out(pta) )
-        else:
-          plt.plot( np.array(pulse.GetTrace()) )
         
-        if analysisFunction != None:
-          analysisFunction(pulse, pta)
-          
-        try:
-          if raw_input() == 'quit': return
-        except KeyboardInterrupt: return
-        except: pass
-        
-        plt.cla()    
+            
         
 
   
