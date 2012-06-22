7fd50032a6d96e709e4811abd005c32e48027878
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2012-06-22 19:36:37 +0000

    adding documentation
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1621 e4a34bab-3947-0410-866c-ab59ef520b34

--- KDataPy/database.py
+++ KDataPy/database.py
@@ -1,14 +1,26 @@
 import couchdbkit
   
 def getdatabase(database, serverName='http://edwdbik.fzk.de:5984'):
+  '''
+  returns an instance of couchdbkit.Database
+  '''
   server = couchdbkit.Server(serverName)
   return server[database]
     
 def kdatadb(serverName='http://edwdbik.fzk.de:5984'):
+  '''
+  returns an instance of couchdbkit.Database initialized to the 'datadb' database
+  '''
   return getdatabase('datadb',serverName)
 
 def kradondb(serverName='http://edwdbik.fzk.de:5984'):
+  '''
+  returns an instance of couchdbkit.Database initialized to the 'radondb' database
+  '''
   return getdatabase('radondb',serverName)
   
 def kcryodb(serverName='http://edwdbik.fzk.de:5984'):
-    return getdatabase('automat',serverName)
+  '''
+  returns an instance of couchdbkit.Database initialized to the 'automat' database
+  '''
+  return getdatabase('automat',serverName)
