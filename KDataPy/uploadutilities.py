27dd9c9e5d353f11000372aa8ee6f4ff4a0bc1ab
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2012-11-17 22:48:48 +0000

    move from util.py to this file… these are different kinds of utilities.  actually, maybe i should move uploadutilities.py into the uploads folder! that makes sense, no?
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1789 e4a34bab-3947-0410-866c-ab59ef520b34

---
+++ KDataPy/uploadutilities.py
@@ -0,0 +1,61 @@
+# format value
+def formatvalue(value):
+  if (isinstance(value,str)):
+    # #see if this string is really an int or a float
+    if value.isdigit()==True: #int
+      return int(value)
+    else: #try a float
+      try:
+        if math.isnan(float(value))==False:
+          return float(value)
+      except:
+        pass
+
+    return value.strip('" ') #strip off any quotations and extra spaces found in the value
+  else:
+    return value
+
+
+#splitargs
+def splitargs(*argv):
+  '''
+  A simple arg parser. This is useful because argparse is not installed on some Python systems with version < 2.7.
+  This supports two kinds of arguments.
+  1. a single value
+  2. a key=value pair. 
+
+  For example, this works
+  "option1 option2 keyword1=value1 keyword2=value2 "
+
+  But this function doesn't support "switches" that have values like "-k 5" 
+  
+  It returns a tuple of ( [args], {kwargs} ).  In the example above, for example it would return
+
+  ( ['option1', 'option2'], {'keyword1':'value1', 'keyword2':'value2'})
+  
+
+  disclaimer:  It is highly recommended that you test this function before completely trusting what it does!!
+  If you want a real tool for parsing arguments, you should use/install argparse.
+
+  '''
+  
+  myargs = []
+  mykwargs = {}
+  for arg in argv:
+    if string.find(arg, '=') == -1:
+      print arg
+      myargs.append(arg)
+    else:
+      key, val = tuple(arg.split('='))
+      
+      if val  == 'True':
+        val = True
+      elif val == 'False':
+        val = False
+      else:
+        val = formatvalue(val)
+     
+      print key,val
+      mykwargs[key] = val
+
+  return (myargs, mykwargs)
