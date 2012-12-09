49194b827ad520604c897342327d85da37fa0940
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2012-12-09 12:52:40 +0000

    adding catch around work... this will be useful to keep the script running if it can't connect to the server for whatever reason
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1823 e4a34bab-3947-0410-866c-ab59ef520b34

--- KDataPy/scripts/uploadToDB/managedUploadRadonData.py
+++ KDataPy/scripts/uploadToDB/managedUploadRadonData.py
@@ -4,6 +4,7 @@
 import sys, os, time, datetime, string, re
 import uploadRadonDataFile as up
 from couchdbkit import Server, Database
+import json
 
 def main(*argv):
   uri = 'https://%s:%s@edelweiss.cloudant.com' % (argv[0], argv[1])
@@ -16,36 +17,45 @@ def main(*argv):
     
     #we get the data file on the database that has the highest
     #run number.
-    s = Server(uri)
-    db = s[dbname]
-    vr = db.view('app/runNumber', reduce =False, limit = 1, descending=True)
-    row = vr.first()
     try:
-      majorRun = row['key'][0]
-      minorRun = row['key'][1]
-    except:
-      majorRun = -1
-      minorRun = -1
-      pass
-    
-    print 'most recent data %d_%d' % (majorRun, minorRun)
-    
-    #now, see if any of the files in the data directory have 
-    #a larger run number
-    files = os.listdir(dataDir)
-    for afile in files:
+      s = Server(uri)
+      db = s[dbname]
+      vr = db.view('app/runNumber', reduce =False, limit = 1, descending=True)
+      row = vr.first()
+      try:
+        majorRun = row['key'][0]
+        minorRun = row['key'][1]
+      except:
+        majorRun = -1
+        minorRun = -1
+        pass
       
-      if prog.match(afile):  
-        majr, minr = afile.strip('.txt').split('_')
-        majr = int(majr)
-        minr = int(minr)
-        #print majr, minr, majorRun, minorRun
-        if (majr > majorRun) or (majr == majorRun and minr > minorRun):
-          print 'uploading', os.path.join(dataDir, afile)
-          up.upload(uri, dbname, os.path.join(dataDir, afile))
+      print 'most recent data %d_%d' % (majorRun, minorRun)
+      
+      #now, see if any of the files in the data directory have 
+      #a larger run number
+      files = os.listdir(dataDir)
+      for afile in files:
+        
+        if prog.match(afile):  
+          majr, minr = afile.strip('.txt').split('_')
+          majr = int(majr)
+          minr = int(minr)
+          #print majr, minr, majorRun, minorRun
+          if (majr > majorRun) or (majr == majorRun and minr > minorRun):
+            print 'uploading', os.path.join(dataDir, afile)
+            up.upload(uri, dbname, os.path.join(dataDir, afile))
 
-    print 'sleeping...', datetime.datetime.now()
-    time.sleep(3600)  #wait one hour to look for more data
+      print 'sleeping...', datetime.datetime.now()
+      time.sleep(3600)  #wait one hour to look for more data
+
+    except Exception as e: 
+      ej = json.loads(e.msg)
+      
+      if ej['error'] == 'unauthorized'
+        pass  #just wait and try again. 
+
+      else raise e  #break - something else happened. 
 
 if __name__ == '__main__':
   main(*sys.argv[1:])
