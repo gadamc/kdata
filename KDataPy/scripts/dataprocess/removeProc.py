22922103e7fe6c64df3e630605a80bcb055a6572
cox <cox@e4a34bab-3947-0410-866c-ab59ef520b34>
2013-01-07 00:34:40 +0000

    adding KDATAPY environment variable to configuration. Changing the removeProc script to use the server-side datadb/_design/proc/_update/removeproc script. See the repository for this design document: https://github.com/gadamc/daqproc.git
    git-svn-id: https://edwdev-ik.fzk.de/svn/EDELWEISS/analysis/kdata/trunk@1830 e4a34bab-3947-0410-866c-ab59ef520b34

--- KDataPy/scripts/dataprocess/removeProc.py
+++ KDataPy/scripts/dataprocess/removeProc.py
@@ -1,70 +1,113 @@
 #!/usr/bin/env python
 
 from couchdbkit import Server, Database
-import sys, string, copy
+from restkit import request
+import sys, string, copy, json
   
 import argparse
 
 
-def deleteFromView(server, databaseName, procname, **kwargs):
+def removeProc(server, databaseName, procname, **kwargs):
   '''
-    This script will remove a "procname" key from database run documents.  You can give this script
-    a single run number, or a range of runs numbers. (For now, it will remove the proc from ALL partition files within
-    a single run. 
-    Additionally, you can specify options to the db.view('proc/daqdoc') call by including key=value pairs to the command line args 
-    kwargs should at least contain startkey and endkey
-  '''
+    This script will remove a "procname" key from database run document and set the doc['status'] = 'good'. 
+    If "procname" == "proc0", then doc['status'] will be set to 'closed'
 
-  s = Server(server)
-  db = s[databaseName]
 
+    You need to set the following arguments:
+    procname = the name of the "procname" object that will be removed from the database documents (proc0, proc1, etc...)
+    server = the full URL to the main database server (https://edelweiss.cloudant.com)
+    databaseName = the name of the database (datadb)
+
+
+    This script will remove the "procname" object from a subset of the database documents. 
+    To specify this particular subset of documents, you must specify the following kwargs:
+    
+    startkey = first run name (ex. "ma22a003_003")
+    endkey = final run name (ex. "mb24b001_010")
+    viewname = the DB View function that you use to get the list of documents. The most general view is "proc/daqdoc". 
+      You can use any of the "Views" that return the run_name + file_name as a key. These are "proc/bad", "proc/daqdoc",
+      "proc/failed", "proc/inprogress", "proc/inqueue", "proc/procX", "proc/procXinprogress" (X = 1,2,3), and "proc/raw"
+
+    All documents that are returned by the viewname within the given startkey/endkey will be affected.
+
+    Optional kwargs:
+
+    status = if you set kwargs['status'] then only database documents with this status will be affected
+    test = if this is set to True, then no documents will be saved back to the database. All other operations will
+      be performed so that you may test the use of this function before affecting docs in the database. 
+
+
+    example: 
+    
+    import removeProc
+    
+    options = {};
+    options['startkey'] = 'mj00a000_000'
+    options['endkey'] = 'zz99z999';
+    options['test'] = True;  #this will just be a test
+    options['viewname'] = 'proc/failed'
+    
+    removeProc.removeProc('https://edelweissuser:password@edelweiss.cloudant.com', 'datadb', proc1', options)
+
+   '''
+
+  #developer note:  In the "proc" _design document now on the database, I've added two "update" function
+  # and a "show" function, which could be used instead of downloading the entire document, updating it and 
+  # then sending the entire thing back to the server
+  #
+  # I should start to use the "updates/in-place" function to put a document on the database (if I can get this to work
+  #  with cloudant -- right now the request.form field is empty!  but for standard couch request.form contains valid data.)
+  #
+
+  db = Server(server)[databaseName]
   
   if kwargs['startkey'] == None or kwargs['endkey'] == None:
-    print 'you should supply a range of runs to use with **kwargs["startkey"] and kwargs["endkey"]'
+    print 'you must supply a range of runs to use. Set **kwargs["startkey"] and kwargs["endkey"]'
     return
-  print kwargs['viewname']
+  print 'using view', kwargs['viewname']
+
+  new_status = 'good'
+  if procname == 'proc0':
+    new_status = 'closed'
+
+    
   vr = db.view(kwargs['viewname'],  reduce = False, startkey=kwargs['startkey'], endkey=kwargs['endkey'])
-  print len(vr)
+  print len(vr), 'rows returned by the view'
 
   for row in vr:
-    if row.has_key('doc'):
-      doc = row['doc']
-    else:
-      doc = db.get(row['id'])
+    print 'checking', row['id']
     
-    print 'checking', doc['_id'], 'status: ', doc['status']
-    if doc.has_key(procname):
-      if kwargs['status'] == '':
-        print 'deleting', procname, 'from', doc['_id']
-        del doc[procname]
-      elif kwargs['status'] == doc['status']:
-        print 'deleting', procname, 'from', doc['_id']
-        del doc[procname]
-      
-      if procname == 'proc0':
-        doc['status'] = 'closed'
-      else:
-        doc['status'] = 'good'
+    reqCom = '%s/%s/_design/proc/_update/removeproc/%s?procname=%s&new_status=%s' % \
+      (server, databaseName, row['id'], procname, new_status )
 
-      if kwargs['test'] == False: db.save_doc(doc)
-      else: print '     debugging: document not saved'
+    if kwargs.has_key('status') and kwargs['status'] != '':
+      reqCom += '&old_status=' + kwargs['status']
+
+    if kwargs.has_key('test') and kwargs['test'] != '':
+      reqCom += '&test=' + str(kwargs['test'])
+
+    print 'HTTP PUT', reqCom
+    resp = request(reqCom, method='PUT')
+    respj = json.loads(resp.body_string())
+    print respj
+
   
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('procname', help='the name of the process key you wish to remove')
-  parser.add_argument('startkey', help='the name of the samba run that defines the beginning of the range of runs (example mj00a000_000')
-  parser.add_argument('endkey', help='the name of the samba run that defines the end of the range of runs (example zz99z999_999')
-  parser.add_argument('-db', '--database', help='select the database you wish to you. "datadb" is the default', default='datadb')
-  parser.add_argument('-s', '--server', help='select the couchdb server you wish to use. You must supply the username and password.', default='https://edelweiss.cloudant.com')
-  parser.add_argument('-v', '--viewname', help='with this option you can specify which couchdb mapReduce View to use to select data files', default='proc/daqdoc')
-  parser.add_argument('-o', '--status', help='documents with the status keyword set to this will only be affected', default='')
-  parser.add_argument('--test', help='only test - do not save document back to database. for debugging', action='store_true')
+  parser.add_argument('procname', help='[required] the name of the process key you wish to remove')
+  parser.add_argument('startkey', help='[required] the name of the samba run that defines the beginning of the range of runs (example mj00a000_000')
+  parser.add_argument('endkey', help='[required] the name of the samba run that defines the end of the range of runs (example zz99z999_999')
+  parser.add_argument('-db', '--database', help='[optional] select the database you wish to you. "datadb" is the default', default='datadb')
+  parser.add_argument('-s', '--server', help='[optional - but required if you need to use a password] select the couchdb server you wish to use. You must supply the username and password.', default='https://edelweiss.cloudant.com')
+  parser.add_argument('-v', '--viewname', help='[optional] with this option you can specify which couchdb mapReduce View to use to select data files', default='proc/daqdoc')
+  parser.add_argument('-o', '--status', help='[optional] documents with the status keyword set to this will only be affected', default='')
+  parser.add_argument('--test', help='[optional] only test - do not save document back to database. for debugging', action='store_true')
   args = parser.parse_args()
 
   print 'Removing', args.procname, 'from database records on', args.server,'/',args.database, 'from startkey', args.startkey, 'to endkey', args.endkey, 'using the view', args.viewname, 'and only documents with status set to: ', args.status, '(empty means all docs)'
 
   if args.test: print '\nThis is only a test.\n'
 
-  deleteFromView(args.server, args.database, args.procname, startkey=args.startkey, endkey=args.endkey, viewname=args.viewname, status=args.status, test=args.test)
+  removeProc(args.server, args.database, args.procname, startkey=args.startkey, endkey=args.endkey, viewname=args.viewname, status=args.status, test=args.test)
 
--- KDataPy/scripts/dataprocess/resetStatus.py
+++ KDataPy/scripts/dataprocess/resetStatus.py
@@ -13,6 +13,34 @@ def resetStatus(server, databaseName, newstatus, **kwargs):
     It will set the 'status' keyword for all documents unless you specify 'oldstatus'=='oldvalue' in the kwargs. If kwargs['oldstatus']
     is given, then only documents with 'status' = 'oldvalue' will be updated to the new status.
     (For now, it will change the status key of ALL partition files within a single run.)
+
+    This script will reset the "status" key from a run document in the database.
+
+    There are three options specified by this function. They are:
+    
+    newstatus = the value of doc['status']
+    server = the full URL to the main database server (https://edelweiss.cloudant.com)
+    databaseName = the name of the database (datadb)
+
+
+    This script will remove the "procname" object from a subset of the database documents. 
+    To specify this particular subset of documents, you must specify the following kwargs:
+    
+    startkey = first run name (ex. "ma22a003_003")
+    endkey = final run name (ex. "mb24b001_010")
+    viewname = the DB View function that you use to get the list of documents. The most general view is "proc/daqdoc". 
+      You can use any of the "Views" that return the run_name + file_name as a key. These are "proc/bad", "proc/daqdoc",
+      "proc/failed", "proc/inprogress", "proc/inqueue", "proc/procX", "proc/procXinprogress" (X = 1,2,3), and "proc/raw"
+
+    All documents that are returned by the viewname within the given startkey/endkey will be affected.
+
+    Optional kwargs:
+
+    status = if you set kwargs['status'] then only database documents with this status will be affected
+    test = if this is set to True, then no documents will be saved back to the database. All other operations will
+      be performed so that you may test the use of this function before affecting docs in the database. 
+
+
   '''
 
   s = Server(server)
@@ -49,7 +77,7 @@ if __name__ == '__main__':
   parser.add_argument('newstatus', help='new status')
   parser.add_argument('startkey', help='the name of the samba run that defines the beginning of the range of runs')
   parser.add_argument('endkey', help='the name of the samba run that defines the end of the range of runs')
-c  parser.add_argument('-db', '--database', help='select the database you wish to use. "datadb" is the default', default='datadb')
+  parser.add_argument('-db', '--database', help='select the database you wish to use. "datadb" is the default', default='datadb')
   parser.add_argument('-s', '--server', help='select the couchdb server you wish to use. You must supply the username and password or this will not work.')
   parser.add_argument('-o', '--oldstatus', help='if this is set, only documents with the current "status" keyword equal to the value given will be updated. otherwise, all documents will have their "status" keyword updated.')
 
