efd41671ee18513fd8e383d9d7a0251596a9451e
gadamc <adam.cox@kit.edu>
2013-02-11 10:26:04 -0800

    adding a mechanism to allow for callers of ManagedSendToLyon class to test a database document and decide if that file should be sent to Lyon. This is used in runProc0 and runMetaProc0 to test for the existence of the _log file, which would indicate that the run has completed. This will mean that the data from s7 will not be transfered to Lyon until the entire run is finished. I only do this to be safe so that all data is copied. it should reduce the likelihood for data to be lost. However, all data management scripts were written to move data over as it arrives on s7. So, in the future, if we want the data faster, we can turn this off.

--- KDataPy/scripts/dataprocess/ManagedSendToLyon.py
+++ KDataPy/scripts/dataprocess/ManagedSendToLyon.py
@@ -7,25 +7,38 @@ import KDataPy.scripts.dataprocess.sftpToSps as sftp
 
 class ManagedSendToLyon:
   '''
-    This class manages sending local data files to Lyon. This class is used both by runProc0 and runMetaProc0. 
+    This class manages sending local data files to Lyon. This class is used both by runProc0 and runMetaProc0. The runProc1
+    script does NOT use this class because it would be more effort to go back and recode this right now.
 
-    To use this class, when you instantiate it, you must provide the name of the database map-reduce "view" and the name of
-    the key of the record that will be attached to the database document to record this action. 
+    To use this class, when you instantiate it, you must provide the name of the database map-reduce "view" that provides the list
+    of database documents that correspond to local data files that will be transferred to Lyon. Also, you must provide the name of
+    the key of the record that will be attached to the database document to record the details of this action. 
 
     Here's a description of what happens when you use this class.
 
     1. Instantiate with object (see __init__ for details of needed values)
-    2. Call the method sendAvailableDocs.
-    3. In this method, the database viewName results are checked. 
+      1b - set the testDocFunction attribute if desired (see Advanced use below)
+    2. Call the method ManagedSendToLyon.sendAvailableDocs.
+    3. In this method, the database mapReduce view results are checked. 
     4. For each document (doc) returned by the view...
-      5. It sets the doc['status'] to 'dbRecordName in progress' and saves the doc to the database
+      5. It sets the doc['status'] to 'dbRecordName in progress' and saves the doc to the database (where dbRecordName = 'proc0' or 'metaproc0' for example)
       6. Sends the local file found in doc['file'] to Lyon via sftp.
       7. Upon successful transfer of the data, sets doc['status'] to 'good' (or to 'dbRecordName failed', if unsuccessful)
       8. Information about the transfer and the file location in Lyon is stored in the doc[dbRecordName] key 
       9. The doc is saved back to the datbase.
 
     This class requires that the documents returned by the database View results has the key 'file', which points
-    to a valid local file. 
+    to a valid local file. That is, it must have doc['file'] for step #6 to work.
+
+    Advanced use:
+          There is an uninitiated attribute to this class called testDocFunction. The user of this class can set this
+          attribute to point to an external function that takes a database document as the input and returns True/False.
+          This lets the user decide if action will be taken for each particular database document. 
+
+          Example: runProc0.py (and runMetaProc0.py) uses this to test for the existence of the _log file in the S7 data directory
+          If the _log file is found, then it assumes that Jules has copied over all of the data for this run and it returns true.
+          When it returns True, then this class proceeds to copy over the data (performing steps 4-9 above)
+
 
 
   '''
@@ -38,9 +51,12 @@ class ManagedSendToLyon:
       password - password for 'username'. this will use sftp.
       viewName - the database viewname in couchdbkit format. That is, for view nameed 'proc0' in the document '_design/proc',
           viewName would be 'proc/proc0'
-      dbRecordName - this is the key name that is given 
+      dbRecordName - this is the key name that is given to the database docs, such as 'proc0' or 'metaproc0'
       limit - if set to true, the method 'sendAvailableDocs' will only send one document, at maximum
       lyonpath - by default set to '/sps/edelweis/kdata/data/raw/'
+
+
+
     '''
 
     self.server = server
@@ -51,6 +67,7 @@ class ManagedSendToLyon:
     self.dbRecordName = dbRecordName  #something like 'proc0'
     self.limit = limit
     self.path = lyonpath
+    self.testDocFunction = None
 
     #create a DBProcess instance, which will assist in uploading the
     #document to the database
@@ -89,6 +106,10 @@ class ManagedSendToLyon:
       
       try:
         doc = self.myProc.get(row['id'])
+
+        if self.testDocFunction is not None:
+          if self.testDocFunction(doc) is False: continue
+
         doc['status'] = self.dbRecordName + ' in progress'
         self.myProc.upload(doc)
 
--- KDataPy/scripts/dataprocess/runProc0.py
+++ KDataPy/scripts/dataprocess/runProc0.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
-import datetime
+import datetime, os, time
 from KDataPy.scripts.dataprocess.ManagedSendToLyon import ManagedSendToLyon
   
+
+def checkForLog(doc):
+  try:
+    dataPath = os.path.dirname(doc['file'])
+    logFilePath = os.path.join(dataPath, os.path.basename(doc['file']).split('_')[0]+'_log')
+
+    if os.path.isfile(logFilePath):
+      time.sleep(300) #sleep five minutes just to make sure all the meta data has copied over.... 
+      return True
+    else:
+      return False
+
+  except: 
+    print str(datetime.datetime.utcnow()), 'runProc0.checkForLog raised an Exception with doc ID %s.... returning false' % doc['_id']
+    return False
+
 def process(*argv):
   '''
   argv[0] is the server (http://127.0.0.1:5984)
@@ -18,6 +34,7 @@ def process(*argv):
   except: pass
 
   theManager = ManagedSendToLyon(argv[0], argv[1], argv[2], argv[3], 'proc/proc0', 'proc0', limit)
+  theManager.testDocFunction = checkForLog  #tell theManager to call this function before it sends any data to Lyon.
 
   print '\n', str(datetime.datetime.utcnow()), ': starting runProc0.py \n'
 
